a515d480c98282274a36a3fc5c072e0b89eaed3a
Timchenko Bogdan <bogdan.timchenko@gmail.com>
2026-02-25 12:23:44 +0200

    FEAT: add migration and model directory paths to settings

--- src/settings.py
+++ src/settings.py
@@ -3,3 +3,7 @@ from pathlib import Path
 BASE_DIR: Path = Path(__file__).resolve().parent
 
 DATABASE_FILE: Path = BASE_DIR / 'database/db.sqlite3'
+
+MIGRATION_DIR: Path = BASE_DIR / 'database/migrations/'
+
+MODEL_DIR: Path = BASE_DIR / 'database/models/'
